dc57dd9ec5670c23661321a5d8d2d51d277f0e50
AndrewWang <AndrewWang0318@outlook.com>
2024-11-19 02:53:48 +0800

    .

--- dist/index.js
+++ dist/index.js
@@ -154,5 +154,5 @@ function _objectSpread(e) { for (var r = 1; r < arguments.length; r++) { var t =
 function _defineProperty(e, r, t) { return (r = _toPropertyKey(r)) in e ? Object.defineProperty(e, r, { value: t, enumerable: !0, configurable: !0, writable: !0 }) : e[r] = t, e; }
 function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
 function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
-var awtool = _objectSpread(_objectSpread(_objectSpread(_objectSpread({}, arrayUtils), cookieUtils), objectUtils), toolUtils);
-var _default = exports["default"] = awtool;
+var jutools = _objectSpread(_objectSpread(_objectSpread(_objectSpread({}, arrayUtils), cookieUtils), objectUtils), toolUtils);
+var _default = exports["default"] = jutools;
